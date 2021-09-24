481b46f9864dfabbf6e28ccef7ba6af21e54da7f
HM <hieumdd@gmail.com>
2021-09-24 22:21:14 +0700

    chore: Removed redundant response

--- main.py
+++ main.py
@@ -14,28 +14,23 @@ def main(request):
     Returns:
         dict: HTTP Response
     """
-        
+
     data = request.get_json()
     print(data)
 
     if "tasks" in data:
-        results = create_tasks(data)
+        response = create_tasks(data)
     elif "view_id" in data and "broadcast" not in data:
-        job = UAJob(
+        response = UAJob(
             headers=data["headers"],
             view_id=data["view_id"],
             website=data["website"],
             principal_content_type=data["principal_content_type"],
             start=data.get("start"),
             end=data.get("end"),
-        )
-        results = job.run()
+        ).run()
     else:
         raise NotImplementedError(data)
 
-    response = {
-        "pipelines": "GA",
-        "results": results,
-    }
     print(response)
     return response
